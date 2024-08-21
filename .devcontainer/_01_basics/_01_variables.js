bcdc5998f4fa52d22901da486edf4eeebe439965
vivekvermad7 <59412867+vivekvermad7@users.noreply.github.com>
2024-08-21 19:20:54 +0000

    Variables

---
+++ .devcontainer/_01_basics/_01_variables.js
@@ -0,0 +1,18 @@
+
+const accoutId = 12345
+var accountEmail = "vivek.verma@gmail.com"
+let accountPassword = 54321
+
+accountCity = "noida"
+
+let accountState;
+
+//accountId = 09876  //cannot be change because we use const(not allowed)
+
+accountEmail = "verma.vivek@gmail.com"
+accountPassword = 1234567890
+accountCity = "Gaya"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
